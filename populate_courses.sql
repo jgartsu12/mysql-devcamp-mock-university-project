4c641f8d5ef85132cda717feaa617070860f758e
jgartsu12 <jgartsu12@gmail.com>
2019-10-02 10:13:15 -0400

    added to courses

---
+++ populate_courses.sql
@@ -0,0 +1,40 @@
+USE devcamp_univ_project_schema;
+
+INSERT INTO courses (courses_title)
+VALUES ("Python");
+
+INSERT INTO courses (courses_title)
+VALUES ("Physics");
+
+INSERT INTO courses (courses_title)
+VALUES ("Calculus");
+
+INSERT INTO courses (courses_title)
+VALUES ("MySQL");
+
+INSERT INTO courses (courses_title)
+VALUES ("Data Science");
+
+INSERT INTO courses (courses_title)
+VALUES ("Algorithms");
+
+INSERT INTO courses (courses_title)
+VALUES ("Computers");
+
+INSERT INTO courses (courses_title)
+VALUES ("Servers");
+
+INSERT INTO courses (courses_title)
+VALUES ("Education");
+
+INSERT INTO courses (courses_title)
+VALUES ("Maths");
+
+INSERT INTO courses (courses_title)
+VALUES ("Medicine");
+
+INSERT INTO courses (courses_title)
+VALUES ("Art");
+
+INSERT INTO courses (courses_title)
+VALUES ("English Language");
