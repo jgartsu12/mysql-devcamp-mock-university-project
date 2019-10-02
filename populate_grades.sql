ef9d9a61706ba55db4969ed781e9cbf992140ff4
jgartsu12 <jgartsu12@gmail.com>
2019-10-02 10:19:55 -0400

    added grades

---
+++ populate_grades.sql
@@ -0,0 +1,35 @@
+INSERT INTO grades (average_grades, top_grades)
+VALUES ("A+", "A+");
+
+INSERT INTO grades (average_grades, top_grades)
+VALUES ("A", "A");
+
+INSERT INTO grades (average_grades, top_grades)
+VALUES ("A-", "A-");
+
+INSERT INTO grades (average_grades, top_grades)
+VALUES ("B+", "B+");
+
+INSERT INTO grades (average_grades, top_grades)
+VALUES ("B", "B");
+
+INSERT INTO grades (average_grades, top_grades)
+VALUES ("B-", "B-");
+
+INSERT INTO grades (average_grades, top_grades)
+VALUES ("C+", "C+");
+
+INSERT INTO grades (average_grades, top_grades)
+VALUES ("C", "C");
+
+INSERT INTO grades (average_grades, top_grades)
+VALUES ("C-", "C-");
+
+INSERT INTO grades (average_grades, top_grades)
+VALUES ("D", "D");
+
+INSERT INTO grades (average_grades, top_grades)
+VALUES ("F", "F");
+
+INSERT INTO grades (average_grades, top_grades)
+VALUES ("I", "I");
