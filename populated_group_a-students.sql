d0a72e907050470b96a81c72952d20929941fa7c
jgartsu12 <jgartsu12@gmail.com>
2019-10-02 12:16:03 -0400

    debug

---
+++ populated_group_a-students.sql
@@ -0,0 +1,4 @@
+SELECT * FROM devcamp_univ_project_schema.students;
+
+INSERT INTO students_group_a_courses
+VALUES ("Harriet Smith", "Jon Snow", "Harry Potter", "Ron Weasly", "Tony Stark", "Tom Riddle", "Hermonie Granger");
---
+++ populated_group_a_students.sql
@@ -0,0 +1,4 @@
+USE devcamp_univ_project_schema;
+
+INSERT INTO students (students_group_a_courses)
+VALUES ("Harriet Smith", "Jon Snow", "Harry Potter", "Ron Weasly", "Tony Stark", "Tom Riddle", "Hermonie Granger");
