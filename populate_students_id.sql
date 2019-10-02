04de6118f0a3b0a64dbe819f5b11094b29627b46
jgartsu12 <jgartsu12@gmail.com>
2019-10-02 10:02:09 -0400

    add students

--- populate_students_id.sql
+++ populate_students_id.sql
@@ -1,4 +1,38 @@
 USE devcamp_univ_project_schema;
 
-INSERT INTO students (students_id) 
-VALUES (1, 2, 3, 4, 5);
+INSERT  INTO students (students_last_name, students_first_name)
+VALUES ("Smith", "Joannah");
+
+INSERT  INTO students (students_last_name, students_first_name)
+VALUES ("Potter", "Harry");
+
+INSERT  INTO students (students_last_name, students_first_name)
+VALUES ("Smith", "Harriet");
+
+INSERT  INTO students (students_last_name, students_first_name)
+VALUES ("Johan", "Sebastian");
+
+INSERT  INTO students (students_last_name, students_first_name)
+VALUES ("Snow", "Jon");
+
+INSERT  INTO students (students_last_name, students_first_name)
+VALUES ("Weasly", "Ron");
+
+INSERT  INTO students (students_last_name, students_first_name)
+VALUES ("Malfoy", "Draco");
+
+INSERT  INTO students (students_last_name, students_first_name)
+VALUES ("Lafleur", "Seb");
+
+INSERT  INTO students (students_last_name, students_first_name)
+VALUES ("Granger", "Hermonie");
+
+INSERT  INTO students (students_last_name, students_first_name)
+VALUES ("Stark", "Tony");
+
+INSERT  INTO students (students_last_name, students_first_name)
+VALUES ("Romanoff", "Natasha");
+
+INSERT  INTO students (students_last_name, students_first_name)
+VALUES ("Riddle", "Tom");
+
